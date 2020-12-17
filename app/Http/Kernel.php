71afb41bbb5dcf675d973729012b82840d448d51
George Alromhin <georgealromhin@gmail>
2020-12-17 18:46:40 +0300

    config

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'locale' => \Illuminate\Auth\Middleware\LocaleMiddleware::class,
-        'amdin' => \Illuminate\Auth\Middleware\AdminAuthMiddleware::class,
+        'admin' => \Illuminate\Auth\Middleware\AdminAuthMiddleware::class,
     ];
 }
